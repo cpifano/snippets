269d4d5c4422085d1b8d5f54d2bbaa36c0562fe9
Camilo Pifano Herrera <camilopifano@gmail.com>
2021-11-03 17:37:40 -0300

    Update Angular snippets

--- MEAN Stack/04 Angular/11 Reactive Forms (Validación).ts
+++ MEAN Stack/04 Angular/11 Reactive Forms (Validación).ts
@@ -4,14 +4,24 @@
 // Es otra herramienta que nos provee el core de Angular desde "@angular/forms", la cual nos permitirá realizar validaciones
 // de formularios.
 //---------------------------------------------------------------------------------------------------------------------------//
+//Para utilizar reactive forms es necesaria su importación en app.module.ts:
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+imports: [
+    //...//
+    FormsModule,
+    ReactiveFormsModule,
+    //...//
+],
+//---------------------------------------------------------------------------------------------------------------------------//
 
 
 //---------------------------------------------------------------------------------------------------------------------------//
 // VISTA DEL COMPONENTE:
 //---------------------------------------------------------------------------------------------------------------------------//
-<form [formGroup]="frmUsuario">
-  <label>Usuario:</label>
-  <input type="text" formControlName="username" [class.error]="frmUsuario.get('username').touched && frmUsuario.get('username').invalid" />
+<form [formGroup]="form" (ngSubmit)="onSubmit()">
+  <input type="text" formControlName="username" />
+  <input type="password" formControlName="password" />
 </form>
 //---------------------------------------------------------------------------------------------------------------------------//
 
@@ -19,15 +29,35 @@
 //---------------------------------------------------------------------------------------------------------------------------//
 // COMPONENTE:
 //---------------------------------------------------------------------------------------------------------------------------//
-//app.component.ts
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+//Importar herramientas para el manejo reactivo del formulario:
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 export class AppComponent {
-  constructor(){}
+  public form: FormGroup;
 
-  frmUsuario = new FormGroup({
-    //El primer parametro corresponde al valor, si no vamos a modificarlo lo dejamos vacío:
-    username = new FormControl('', Validators.required)
-  });
+  constructor(){
+    //Inicializar FormGroup y establecer reglas de validación:
+    this.form = new FormGroup({
+      //El primer parametro corresponde al valor, si no vamos a modificarlo lo dejamos vacío:
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    //CASO UPDATE:
+    //Cargar datos al formulario:
+    this.form.setValue({
+      username: res.data.username,
+      password: res.data.password,
+    });
+  }
+
+  onSubmit(){
+    //Validar campos:
+    if(this.form.valid){
+      guardar(this.form.value);
+    }
+  }
 }
 //---------------------------------------------------------------------------------------------------------------------------//
