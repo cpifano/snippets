c32fa6b5db20755acd14d26a640ec5d4f2106400
cpifano <camilopifano@gmail.com>
2023-01-20 15:54:44 -0300

    Update NodeJS snippets

---
+++ MEAN Stack/02 NodeJS/18 Nodemon.js
@@ -0,0 +1,9 @@
+//---------------------------------------------------------------------------------------------------------------------------//
+//Instalar nodemon (Herramienta para desarrollo):
+npm install -g nodemon
+
+// En lugar de ejecutar un archivo con el comando node, lo hacemos con nodedemon:
+// De esta forma si ejecutamos un cambio en el contenido de nuestra implementación web, se verá reflejado automáticamente,
+// ya que nodedemon se encargará de reiniciar el servicio cada vez que guardemos nuestro archivo.
+nodemon nombre_archivo
+//---------------------------------------------------------------------------------------------------------------------------//
