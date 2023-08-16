571e902466567fc8d519f0bb51ceaa68d765d184
Camilo Pifano <camilopifano@gmail.com>
2023-08-16 15:23:21 -0300

    Webpack in NodeJS

--- MEAN Stack/02 NodeJS/41 Webpack.js
+++ MEAN Stack/02 NodeJS/41 Webpack.js
@@ -1,7 +1,10 @@
-// Install Webpack:
+//---------------------------------------------------------------------------------------------------------------------------//
+// WEBPACK:
+//---------------------------------------------------------------------------------------------------------------------------//
+// Instalar Webpack como dependencia de desarrollo:
 npm install webpack webpack-cli --save-dev
 
-//Agregar al archivo package.json el parametro private:
+// Agregar al archivo package.json el parametro private:
 //"main": "index.js",
 "private":true,
 //"scripts": { ... }
@@ -18,5 +21,52 @@ module.exports = {
 	}
 }
 
-// Build with Webpack:
+// Transpilar con Webpack:
 npx webpack --config webpack.config.js
+//---------------------------------------------------------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------------------------------------------------------//
+// WEBPACK 5+ (Polyfill):
+//---------------------------------------------------------------------------------------------------------------------------//
+// PLUGIN SOLUTION:
+//Instalar node-polyfill-webpack-plugin:
+npm install node-polyfill-webpack-plugin --save-dev
+
+//Importar node-polyfill-webpack-plugin:
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+//Setear node-polyfill-webpack-plugin:
+module.exports = {
+  // ...
+  plugins: [new NodePolyfillPlugin()],
+}
+
+// FALLBACK SOLUTION:
+// Otro camino para evitar los errores de polyfill es usar la propiedad 'resolve fallback'
+// con los elementos que estén generando error durante la transpilación.
+module.exports = {
+    //...//
+    resolve	: {
+		fallback : {
+			"querystring": false,
+			"canvas": false,
+			"tls": false,
+			"net": false,
+			"dns": false,
+			"child_process": false,
+		
+			fs		: require.resolve('browserify-fs'),
+			http	: require.resolve("stream-http"),
+			https	: require.resolve("https-browserify"),
+			os		: require.resolve("os-browserify/browser"),
+			crypto	: require.resolve("crypto-browserify"),
+			stream	: require.resolve("stream-browserify"),
+			zlib	: require.resolve("browserify-zlib"),
+			vm		: require.resolve("vm-browserify"),
+		}
+	}
+}
+
+//Instalar browserify-fs para resolver dependencia de fs:
+npm install browserify-fs --save-dev
+//---------------------------------------------------------------------------------------------------------------------------//
